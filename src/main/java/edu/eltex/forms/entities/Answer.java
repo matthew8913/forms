deaa3a0f0f701d08ccbab2fcbc9015a478321d6d
Юркевич Матвей <matvey.yurkevich@gmail.com>
2024-12-01 22:49:26 +0700

    feat: add entities

---
+++ src/main/java/edu/eltex/forms/entities/Answer.java
@@ -0,0 +1,33 @@
+package edu.eltex.forms.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "answers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "response_id", nullable = false)
+    private Response response;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id", nullable = false)
+    private Question question;
+
+    private String answerText;
+
+    @ManyToOne
+    @JoinColumn(name = "selected_option_id")
+    private Option selectedOption;
+}
---
+++ src/main/java/edu/eltex/forms/entities/Response.java
@@ -0,0 +1,27 @@
+package edu.eltex.forms.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "responses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Response {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "form_id", nullable = false)
+    private Form form;
+}
