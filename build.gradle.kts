a86921a785449eaa6db9e60ce5d8d9ce6e3f2961
Ермолаев Александр <sassha.eer@gmail.com>
2024-12-01 20:24:11 +0700

    FEAT: project created

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.0"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "edu.eltex"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	// implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	// implementation("org.springframework.boot:spring-boot-starter-security")
+	// implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
+	// implementation("org.liquibase:liquibase-core")
+	compileOnly("org.projectlombok:lombok")
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:postgresql")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	// testImplementation("org.springframework.security:spring-security-test")
+	// implementation(kotlin("script-runtime"))
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
