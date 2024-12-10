5369efbb2861289acf991bb9f23b67d3b8094880
Юркевич Матвей <matvey.yurkevich@gmail.com>
2024-12-10 14:29:55 +0700

    #1936 feat: make statistic page beautiful

--- ui/src/views/Statistics.vue
+++ ui/src/views/Statistics.vue
@@ -1,33 +1,86 @@
 <template>
     <div class="container mt-5">
         <h1 class="text-center mb-4">Статистика</h1>
-        <div v-if="statistic">
-            <div class="mb-4">
-                <h3>Общее количество завершений: {{ statistic.numberOfCompletions }}</h3>
+        <div v-if="statistic" class="row">
+            <div class="col-12 mb-4">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <h3 class="card-title">Пользователей, прошедших опрос: {{ statistic.numberOfCompletions }}</h3>
+                    </div>
+                </div>
             </div>
-            <div v-for="(questionStat, index) in statistic.questionStatistic" :key="index" class="mb-4">
-                <h4>Вопрос {{ index + 1 }}: {{ questionStat.questionText }}</h4>
-                <div v-if="questionStat.statistic.answers && questionStat.statistic.minAnswer !== undefined">
-                    <h5>Числовая статистика</h5>
-                    <p>Минимум: {{ questionStat.statistic.minAnswer }}</p>
-                    <p>Максимум: {{ questionStat.statistic.maxAnswer }}</p>
-                    <p>Среднее: {{ questionStat.statistic.avgAnswer }}</p>
-                    <p>Ответы: {{ questionStat.statistic.answers.join(', ') }}</p>
-                </div>
-                <div v-else-if="questionStat.statistic.percentageOfAnswered">
-                    <h5>Статистика выбора</h5>
-                    <ul>
-                        <li v-for="(answer, idx) in questionStat.statistic.answers" :key="idx">
-                            {{ answer }} - {{ questionStat.statistic.numberOfAnswered[idx] }} ответов ({{
-                                questionStat.statistic.percentageOfAnswered[idx] }}%)
-                        </li>
-                    </ul>
-                </div>
-                <div v-else>
-                    <p>Нет данных для этого вопроса.</p>
+
+            <div v-for="(questionStat, index) in statistic.questionStatistic" :key="index" class="col-12 mb-4">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <h4 class="card-title">Вопрос {{ index + 1 }}: {{ questionStat.questionText }}</h4>
+
+                        <div v-if="questionStat.statistic.answers && questionStat.statistic.minAnswer !== undefined"
+                            class="mt-3">
+                            <h5 class="text-success">Числовая статистика</h5>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <p><strong>Минимум:</strong> {{ questionStat.statistic.minAnswer }}</p>
+                                    <p><strong>Максимум:</strong> {{ questionStat.statistic.maxAnswer }}</p>
+                                    <p><strong>Среднее:</strong> {{ questionStat.statistic.avgAnswer }}</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <p><strong>Ответы:</strong></p>
+                                    <div class="overflow-hidden" style="max-height: 50px;">
+                                        <span>{{ formattedAnswers(questionStat.statistic.answers) }}</span>
+                                    </div>
+                                    <button class="btn btn-link p-0" @click="toggleAnswers(index)">
+                                        {{ showFullAnswers[index] ? 'Скрыть' : 'Показать все' }}
+                                    </button>
+                                    <div v-if="showFullAnswers[index]" class="mt-2">
+                                        <ul class="list-unstyled">
+                                            <li v-for="(answer, idx) in questionStat.statistic.answers" :key="idx">
+                                                {{ answer }}
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div v-else-if="questionStat.statistic.percentageOfAnswered" class="mt-3">
+                            <h5 class="text-success">Статистика выбранных вариантов</h5>
+                            <table class="table table-bordered table-hover">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th>Ответ</th>
+                                        <th>Количество ответов</th>
+                                        <th>Процент</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="(answer, idx) in questionStat.statistic.answers" :key="idx">
+                                        <td>{{ answer }}</td>
+                                        <td>{{ questionStat.statistic.numberOfAnswered[idx] }}</td>
+                                        <td>
+                                            <div class="progress">
+                                                <div class="progress-bar" role="progressbar"
+                                                    :style="{ width: questionStat.statistic.percentageOfAnswered[idx] + '%' }"
+                                                    :aria-valuenow="questionStat.statistic.percentageOfAnswered[idx]"
+                                                    aria-valuemin="0" aria-valuemax="100">
+                                                    {{ questionStat.statistic.percentageOfAnswered[idx] }}%
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div v-else class="mt-3">
+                            <p class="text-muted">Нет данных для этого вопроса.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <!-- Загрузка -->
         <div v-else class="text-center">
             <p>Загрузка статистики...</p>
         </div>
@@ -42,10 +95,11 @@ export default {
     data() {
         return {
             statistic: null,
+            showFullAnswers: {},
         };
     },
     async created() {
-        const formId = this.$route.params.formId; 
+        const formId = this.$route.params.formId;
         await this.fetchStatistics(formId);
     },
     methods: {
@@ -64,6 +118,16 @@ export default {
             } catch (error) {
                 console.error('Ошибка при загрузке статистики:', error);
             }
+        },
+        formattedAnswers(answers) {
+            const sortedAnswers = answers.sort((a, b) => a - b);
+            if (sortedAnswers.length > 5) {
+                return sortedAnswers.slice(0, 5).join(', ') + '...';
+            }
+            return sortedAnswers.join(', ');
+        },
+        toggleAnswers(index) {
+            this.$set(this.showFullAnswers, index, !this.showFullAnswers[index]);
         },
     },
 };
