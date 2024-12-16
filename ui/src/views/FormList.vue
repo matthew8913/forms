af550079cf26ddbe67ed72f5e1ffe244e103c6e2
Юркевич Матвей <matvey.yurkevich@gmail.com>
2024-12-16 18:19:48 +0700

    #1983 fix: process bad logs

--- ui/src/views/FormList.vue
+++ ui/src/views/FormList.vue
@@ -1,88 +1,79 @@
 <template>
-    <div class="container mt-5">
-      <h1 class="text-center mb-4">Список опросов</h1>
-      <div v-if="userRole === 'CREATOR'" class="text-center mb-4">
-        <router-link to="/create-form" class="btn-link">
-          <span class="btn-link-text">Создать опрос</span>
-        </router-link>
-      </div>
-      <div v-if="forms.length === 0" class="text-center">
-        <p>Нет доступных опросов</p>
-      </div>
-      <div v-else>
-        <div v-for="form in forms" :key="form.id" class="mb-3">
-          <div class="card" :class="{ 'bg-light': form.completed }">
-            <div class="card-body">
-              <h5 class="card-title">{{ form.title }}</h5>
-              <p class="card-text">{{ form.description }}</p>
-              <div v-if="userRole === 'USER'">
-                <router-link
-                  v-if="!form.completed"
-                  :to="`/complete-form/${form.id}`"
-                  class="btn btn-primary"
-                >
-                  Заполнить опрос
-                </router-link>
-                <router-link
-                  v-else
-                  :to="`/my-answers/${form.completionId}`"
-                  class="btn btn-secondary"
-                >
-                  Посмотреть мои ответы
-                </router-link>
-              </div>
-              <div v-else-if="userRole === 'CREATOR'">
-                <router-link
-                  :to="`/statistics/${form.id}`"
-                  class="btn btn-primary"
-                >
-                  Посмотреть статистику
-                </router-link>
-              </div>
+  <div class="container mt-5">
+    <h1 class="text-center mb-4">Список опросов</h1>
+    <div v-if="userRole === 'CREATOR'" class="text-center mb-4">
+      <router-link to="/create-form" class="btn-link">
+        <span class="btn-link-text">Создать опрос</span>
+      </router-link>
+    </div>
+    <div v-if="forms.length === 0" class="text-center">
+      <p>Нет доступных опросов</p>
+    </div>
+    <div v-else>
+      <div v-for="form in forms" :key="form.id" class="mb-3">
+        <div class="card" :class="{ 'bg-light': form.completed }">
+          <div class="card-body">
+            <h5 class="card-title">{{ form.title }}</h5>
+            <p class="card-text">{{ form.description }}</p>
+            <div v-if="userRole === 'USER'">
+              <router-link v-if="!form.completed" :to="`/complete-form/${form.id}`" class="btn btn-primary">
+                Заполнить опрос
+              </router-link>
+              <router-link v-else :to="`/my-answers/${form.completionId}`" class="btn btn-secondary">
+                Посмотреть мои ответы
+              </router-link>
+            </div>
+            <div v-else-if="userRole === 'CREATOR'">
+              <router-link :to="`/statistics/${form.id}`" class="btn btn-primary">
+                Посмотреть статистику
+              </router-link>
             </div>
           </div>
         </div>
-
       </div>
+
     </div>
-  </template>
-  
-  <script>
-  import { API_BASE_URL } from '../../config.js';
-  import { authService } from '../service/authService.js';
-  
-  export default {
-    data() {
-      return {
-        forms: [],
-        userRole: null,
-      };
-    },
-    async created() {
-      this.userRole = authService.getUserRole();
-      await this.fetchForms();
-    },
-    methods: {
-      // Метод сбора форм
-      async fetchForms() {
-        try {
-          const response = await authService.fetchWithToken(`${API_BASE_URL}/forms`);
-          if (!response.ok) {
-            const errorData = await response.json();
-            throw new Error(errorData);
-          }
-          const data = await response.json();
-          // Проверка на прохождение пользователем каждого опроса (для корректного отображения)
+  </div>
+</template>
+
+<script>
+import { API_BASE_URL } from '../../config.js';
+import { authService } from '../service/authService.js';
+
+export default {
+  data() {
+    return {
+      forms: [],
+      userRole: null,
+    };
+  },
+  async created() {
+    this.userRole = authService.getUserRole();
+    await this.fetchForms();
+  },
+  methods: {
+    // Метод сбора форм
+    async fetchForms() {
+      try {
+        const response = await authService.fetchWithToken(`${API_BASE_URL}/forms`);
+        if (!response.ok) {
+          const errorData = await response.json();
+          throw new Error(errorData);
+        }
+        const data = await response.json();
+        if (authService.getUserRole === 'USER') {
           for (const form of data) {
             await this.checkFormCompletion(form);
           }
-          this.forms = data;
-        } catch (error) {
-          console.error('Ошибка при загрузке списка опросов:', error);
         }
-      },
-      // Метод проверки прохождения опроса
-      async checkFormCompletion(form) {
+        this.forms = data;
+      } catch (error) {
+        console.error('Ошибка при загрузке списка опросов:', error);
+      }
+    },
+
+    async checkFormCompletion(form) {
+      try {
         const userId = authService.getUserId();
         const response = await authService.fetchWithToken(
           `${API_BASE_URL}/completions/user-form-completion?formId=${form.id}&userId=${userId}`
@@ -92,15 +83,22 @@
           form.completed = true;
           form.completionId = completionId;
         } else {
+          if (response.status !== 404) {
+            const errorData = await response.json();
+            throw new Error(errorData);
+          }
           form.completed = false;
         }
-      },
+      } catch (error) {
+        console.error('Ошибка при проверке прохождения опроса:', error);
+      }
     },
-  };
-  </script>
-  
-  <style scoped>
-  .bg-light {
-    opacity: 0.6;
-  }
-  </style>
+  },
+};
+</script>
+
+<style scoped>
+.bg-light {
+  opacity: 0.6;
+}
+</style>
