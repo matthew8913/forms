9c6b4c6bc22e494ce03d346f36df57797474161f
Юркевич Матвей <matvey.yurkevich@gmail.com>
2024-12-16 17:38:50 +0700

    #1983 fix: fix cors bug

--- src/main/java/edu/eltex/forms/WebConfig.java
+++ src/main/java/edu/eltex/forms/WebConfig.java
@@ -1,10 +1,14 @@
 package edu.eltex.forms;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -65,16 +69,4 @@ public class WebConfig implements WebMvcConfigurer {
                 });
     }
 
-    /**
-     * Настройка CORS (Cross-Origin Resource Sharing)
-     * @param registry регистратор
-     */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .allowCredentials(false);
-    }
 }
